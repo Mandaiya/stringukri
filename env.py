e272e7a237f168b5b1341aa99dfd1a3d6a7ebded
natha <108113193+Mandaiya@users.noreply.github.com>
2025-04-03 04:08:18 +0530

    Update env.py

--- env.py
+++ env.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID", "24401235").strip()
 API_HASH = os.getenv("API_HASH", "149f7e13d7d861b27cffc3ab1fd52b22").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6819381670:AAHf6ZRYiHrTunC_NAgprMHGKdTcmbJt_S0").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7476307432:AAETfjWyX_8-58DIiuXF-eIL1VaIj_VQepw").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Filmy_tube")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Puthusa_yosi")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
